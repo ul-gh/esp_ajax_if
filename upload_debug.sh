fa8066e2d2b80d4692292016965c1faef14ad95b
Ulrich Lukas <uli@domain.invalid>
2020-10-13 02:42:47 +0200

    hmm

--- upload_debug.sh
+++ upload_debug.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
+# Only write firmware image as bootloader does not fit when compiled with -O0
 
 esptool --chip esp32 --baud 921600 --before default_reset --after hard_reset \
     write_flash -z --flash_mode dio --flash_freq 40m --flash_size detect \
-    0x1000 bootloader.bin \
-    0x18000 partitions.bin \
-    0x20000 firmware.bin \
-    0x300000 spiffs.bin
+    0x20000 .pio/build/debug/firmware.bin \
