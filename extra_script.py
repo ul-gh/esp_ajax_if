85f3720aea8d399e939954e2aa867d9a7dca2ec5
Ulrich Lukas <uli@domain.invalid>
2020-10-16 01:02:37 +0200

    dbg

--- extra_script.py
+++ extra_script.py
@@ -13,32 +13,32 @@ env.Append(CXXFLAGS=["-std=gnu++17"]) # We use C++ 17
 
 
 ################################################################################
-# Ulrich Lukas 2020-09-19: Account for changed bootloader and partition offsets
-if "upload" in COMMAND_LINE_TARGETS and "uploadfs" not in COMMAND_LINE_TARGETS:
-    print("UL ===> Using custom upload command for non-standard partitioning scheme...")
-    # From .platformio/platforms/espressif32/builder/frameworks/espidf.py
-    env.Replace(FLASH_EXTRA_IMAGES=[
-                ("0x1000", os.path.join("$BUILD_DIR", "bootloader.bin")),
-                ("0x18000", os.path.join("$BUILD_DIR", "partitions.bin")),
-                ])
-    # From .platformio/platforms/espressif32/builder/main.py
-    env.Replace(UPLOADERFLAGS=[
-                "--chip", "esp32",
-                "--port", '"$UPLOAD_PORT"',
-                "--baud", "$UPLOAD_SPEED",
-                "--before", "default_reset",
-                "--after", "hard_reset",
-                "write_flash", "-z",
-                "--flash_mode", "${__get_board_flash_mode(__env__)}",
-                "--flash_freq", "${__get_board_f_flash(__env__)}",
-                "--flash_size", "detect",
-                ])
-    # From .platformio/platforms/espressif32/builder/main.py
-    for image in env.get("FLASH_EXTRA_IMAGES", []):
-        env.Append(UPLOADERFLAGS=[image[0], env.subst(image[1])])
-        env.Replace(UPLOADCMD='"$PYTHONEXE" "$UPLOADER" $UPLOADERFLAGS 0x20000 $SOURCE')
-
-
+# # Ulrich Lukas 2020-09-19: Account for changed bootloader and partition offsets
+# if "upload" in COMMAND_LINE_TARGETS and "uploadfs" not in COMMAND_LINE_TARGETS:
+#     print("UL ===> Using custom upload command for non-standard partitioning scheme...")
+#     # From .platformio/platforms/espressif32/builder/frameworks/espidf.py
+#     env.Replace(FLASH_EXTRA_IMAGES=[
+#                 ("0x1000", os.path.join("$BUILD_DIR", "bootloader.bin")),
+#                 ("0x18000", os.path.join("$BUILD_DIR", "partitions.bin")),
+#                 ])
+#     # From .platformio/platforms/espressif32/builder/main.py
+#     env.Replace(UPLOADERFLAGS=[
+#                 "--chip", "esp32",
+#                 "--port", '"$UPLOAD_PORT"',
+#                 "--baud", "$UPLOAD_SPEED",
+#                 "--before", "default_reset",
+#                 "--after", "hard_reset",
+#                 "write_flash", "-z",
+#                 "--flash_mode", "${__get_board_flash_mode(__env__)}",
+#                 "--flash_freq", "${__get_board_f_flash(__env__)}",
+#                 "--flash_size", "detect",
+#                 ])
+#     # From .platformio/platforms/espressif32/builder/main.py
+#     for image in env.get("FLASH_EXTRA_IMAGES", []):
+#         env.Append(UPLOADERFLAGS=[image[0], env.subst(image[1])])
+#         env.Replace(UPLOADCMD='"$PYTHONEXE" "$UPLOADER" $UPLOADERFLAGS 0x20000 $SOURCE')
+# 
+# 
 ################################################################################
 # Create ZIP archive containing firmware binary images for stand-alone deployment
 def zip_bin(target, source, env):
